3ffe0338727ae3e91ae8f61643e3e12e7602c1f5
PRIYANSHDUBEY <87897527+PRIYANSHDUBEY@users.noreply.github.com>
2023-08-24 19:27:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+() => {
+  cy.get('td') // Selects all <td> elements on the page
+    .should('have.css', 'padding', '1px') // Asserts that the selected elements have a CSS property 'padding' with a value of '1px'
+    .should('have.css', 'border', '1px solid rgb(0, 0, 0)'); // Asserts that the selected elements have a CSS property 'border' with a value of '1px solid rgb(0, 0, 0)'
+}
